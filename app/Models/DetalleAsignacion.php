eedf95185ec36428d1d6bb153c2415a5b16b36b9
Gustavo <administrador@sqlapio.com>
2023-11-10 15:54:48 -0400

    Modificaciones en cabinas y ventas

--- app/Models/DetalleAsignacion.php
+++ app/Models/DetalleAsignacion.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class DetalleAsignacion extends Model
 {
@@ -32,4 +33,9 @@ class DetalleAsignacion extends Model
         'fecha',
         'status',
     ];
+
+    public function get_detalle_asiganciones(): HasMany
+    {
+        return $this->hasMany(Cliente::class, 'cliente_id', 'id');
+    }
 }

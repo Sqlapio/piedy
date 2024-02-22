19b21e9d0596711f99ec2b8f09d85a3bc1117818
Gustavo <administrador@sqlapio.com>
2024-02-22 10:05:36 -0400

    Correcciones en el correo de cierre

--- resources/views/emails/cierre_diario.blade.php
+++ resources/views/emails/cierre_diario.blade.php
@@ -2,6 +2,31 @@
 <html>
 <head>
     <title>Piedy.com</title>
+    <style>
+        #customers {
+        font-family: Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+        }
+
+        #customers td, #customers th {
+        border: 1px solid #ddd;
+        padding: 8px;
+        }
+
+        #customers tr:nth-child(even){background-color: #f2f2f2;}
+
+        #customers tr:hover {background-color: #ddd;}
+
+        #customers th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #7898a5;
+        color: black;
+        }
+
+    </style>
 </head>
 <body>
     <img style="display: block; margin-left: auto; margin-right: auto; width: 600px; height: auto;" src="{{ asset('images/banner_correo.jpg') }}">
@@ -13,8 +38,59 @@
             Se informa que el cierre de caja fue ejecutado con éxito. <br>
             A continuación, encontrará el detalle del cierre para su referencia:
             <br>
+            <br>
+            <strong>TASA BCV: {{ $mailData['tasa_bcv'] }}</strong>
+            <br>
         </p>
-        <p style="text-align: justify; margin-left: 20px;">
+        <div style="margin: auto; width: 600px; padding: 10px;">
+            <table id="customers">
+                <tr>
+                    <th>Denominación</th>
+                    <th>Valor</th>
+                </tr>
+                <tr>
+                    <td>Responsable</td>
+                    <td>{{ $mailData['responsable'] }}</td>
+                </tr>
+                <tr>
+                    <td>Fecha</td>
+                    <td>{{ $mailData['fecha'] }}</td>
+                </tr>
+                <tr>
+                    <td>Total General de Ventas($)</td>
+                    <td>{{ $mailData['total_ventas'] }}</td>
+                </tr>
+                <tr>
+                    <td>Total($)</td>
+                    <td>{{ $mailData['total_dolares'] }}</td>
+                </tr>
+                <tr>
+                    <td>Zelle($)</td>
+                    <td>{{ $mailData['zelle'] }}</td>
+                </tr>
+                <tr>
+                    <td>Total(Bs.)</td>
+                    <td>{{ $mailData['total_bolivares'] }}</td>
+                </tr>
+                <tr>
+                    <td>Conversión (Bs.) -> ($)</td>
+                    <td>{{ $mailData['conversion'] }}</td>
+                </tr>
+                <tr>
+                    <td>Efectivo($) en caja</td>
+                    <td>{{ $mailData['efectivo_caja_usd'] }}</td>
+                </tr>
+                <tr>
+                    <td>Gastos($)</td>
+                    <td>{{ $mailData['gastos'] }}</td>
+                </tr>
+                <tr>
+                    <td>Saldo Caja Chica($)</td>
+                    <td>{{ $mailData['efectivo_caja_chica'] }}</td>
+                </tr>
+            </table>
+        </div>
+        {{-- <p style="text-align: justify; margin-left: 20px;">
             <h3 style="text-align: justify; margin-left: 20px;">
                 Responsable: {{ $mailData['responsable'] }}
                 <br>
@@ -36,7 +112,7 @@
                 <br>
                 Saldo Caja Chica($): {{ $mailData['efectivo_caja_chica'] }}
             </h3>
-        </p>
+        </p> --}}
         <p style="text-align: justify; margin-left: 20px;">
             Esta notificación confirma que el proceso de facturación asociado a su trabajo ha sido completado correctamente. <br>
             Agradecemos su esfuerzo continuo y la dedicación con la que lleva a cabo cada servicio, lo cual es fundamental para nuestro compromiso con la excelencia. <br>

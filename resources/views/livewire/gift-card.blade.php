0864a553ef28421f4194a9b2463d87ccd1b05c68
Gustavo <administrador@sqlapio.com>
2024-05-02 12:32:10 -0400

    se agrega modelo de tarjeta

--- resources/views/livewire/gift-card.blade.php
+++ resources/views/livewire/gift-card.blade.php
@@ -4,7 +4,62 @@
         <h1 class="text-xl mb-6 font-bold text-black">Asignación de GiftCard</h1>
         <div class="flex justify-between items-center gap-x-8">
             <div class="w-full">
-                <img src="{{ asset('images/gift-card-black.png') }}" alt="">
+                <div class="w-96 h-56 m-auto bg-red-100 rounded-xl relative text-white shadow-2xl transition-transform transform hover:scale-110">
+
+                    <img class="relative object-cover w-full h-full rounded-xl" src="https://i.imgur.com/kGkSg1v.png">
+
+                    <div class="w-full px-8 absolute top-8">
+                        <div class="flex justify-between">
+                            <div class="">
+                                <p class="font-light">
+                                    Name
+                                </h1>
+                                <p class="font-medium tracking-widest">
+                                    Karthik P
+                                </p>
+                            </div>
+                            <img class="w- h-auto" src="{{ asset('images/logo.png') }}"/>
+                        </div>
+                        <div class="pt-1">
+                            <p class="font-light">
+                                Card Number
+                            </h1>
+                            <p class="font-medium tracking-more-wider">
+                                4642  3489  9867  7632
+                            </p>
+                        </div>
+                        <div class="pt-6 pr-6">
+                            <div class="flex justify-between">
+                                <div class="">
+                                    <p class="font-light text-xs">
+                                        Valid
+                                    </h1>
+                                    <p class="font-medium tracking-wider text-sm">
+                                        11/15
+                                    </p>
+                                </div>
+                                <div class="">
+                                    <p class="font-light text-xs text-xs">
+                                        Expiry
+                                    </h1>
+                                    <p class="font-medium tracking-wider text-sm">
+                                        03/25
+                                    </p>
+                                </div>
+
+                                <div class="">
+                                    <p class="font-light text-xs">
+                                        CVV
+                                    </h1>
+                                    <p class="font-bold tracking-more-wider text-sm">
+                                        ···
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
             <div class="w-full">
                 {{-- linea 1 --}}

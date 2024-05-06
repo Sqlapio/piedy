d34f07b5e321ceff00169a2e1e65826b33a0f487
Gustavo <administrador@sqlapio.com>
2024-05-06 18:00:55 -0400

    logica de giftcard v4

--- resources/views/livewire/gift-card.blade.php
+++ resources/views/livewire/gift-card.blade.php
@@ -3,7 +3,7 @@
     <div class="p-8 bg-[#e9d4cf] rounded-xl mb-5">
         <h1 class="text-xl mb-6 font-bold text-black">Asignación de GiftCard</h1>
         <!-- Formulario para la Gifcar -->
-        <div class="flex justify-between items-center gap-x-8">
+        <div class="flex justify-between sm:flex-col items-center gap-x-8">
             <div class="w-full">
                 <div class="w-full h-auto m-auto bg-red-100 rounded-xl relative text-white shadow-2xl transition-transform transform hover:scale-110 ">
 
@@ -66,7 +66,7 @@
             </div>
             <div class="w-full p-4">
                 {{-- linea 1 --}}
-                <div class="grid md:grid-cols-2 md:gap-6">
+                <div class="grid sm:grid-cols-1 md:grid-cols-2 md:gap-6">
                     <div class="w-full mb-6 group">
                         <label class="mb-1 block text-md text-black text-left">Código GiftCard</label>
                         <x-input wire:model.live="cod_gift_card" right-icon="user" disabled/>
@@ -80,7 +80,7 @@
                     </div>
                 </div>
                 {{-- linea 2 --}}
-                <div class="grid md:grid-cols-3 md:gap-6 mt-5">
+                <div class="grid sm:grid-cols-1 md:grid-cols-3 md:gap-6 mt-5">
                     <div class="w-full mb-6 group hidden">
                         <label class="mb-1 block text-md text-black text-left">Fecha de emición</label>
                         <x-input wire:model.live="fecha_emicion" right-icon="user" disabled/>
@@ -104,7 +104,7 @@
                 </div>
             </div>
         </div>
-        <div class="flex justify-end p-2 mt-auto">
+        <div class="flex md:justify-end p-2 mt-auto">
             <button type="submit" wire:click.prevent="store()" class="justify-end rounded-md border border-transparent bg-[#7898a5] py-2 px-4 text-sm font-bold text-white shadow-sm hover:bg-check-green">
                 <svg xmlns="http://www.w3.org/2000/svg" wire:loading wire:target="store" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="animate-spin h-5 w-5 mr-3">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />

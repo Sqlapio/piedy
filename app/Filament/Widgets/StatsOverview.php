450e9d2b202acfabc09c43642305b8c17f64d5f1
Gustavo <administrador@sqlapio.com>
2024-06-29 12:57:25 -0400

    agregando stats al dashboard principal v3

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -7,44 +7,52 @@ use App\Models\Producto;
 use App\Models\VentaServicio;
 use App\Models\Disponible;
 use App\Models\Frecuencia;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 
 class StatsOverview extends BaseWidget
 {
-    // protected static ?string $pollingInterval = '10s';
-
+    use InteractsWithPageFilters;
 
     protected function getStats(): array
     {
+        if($this->filters['activar'] == false)
+        {
+            $rangeStartDate = now()->startOfYear();
+            $rangeEndDate = now()->endOfYear();
+        }else{
+            $rangeStartDate = $this->filters['startDate'].' 00:00:00.000';
+            $rangeEndDate = $this->filters['endDate'].'. 23:59:59.000';
+        }
+
+        $ventas_usd = VentaServicio::whereBetween('created_at',[$rangeStartDate, $rangeEndDate])->sum('total_USD');
+        $ventas_bsd = VentaServicio::whereBetween('created_at',[$rangeStartDate, $rangeEndDate])->sum('pago_bsd');
+        $servicios = VentaServicio::whereBetween('created_at',[$rangeStartDate, $rangeEndDate])->count();
+
         return [
 
-            Stat::make('Ventas($)', '$ '.number_format(VentaServicio::sum('total_USD'), 2, '.', ','))
+            Stat::make('Ventas($)', '$ '.number_format($ventas_usd, 2, '.', ','))
                 ->description('Neto de ventas en Dolares')
                 ->descriptionIcon('heroicon-m-presentation-chart-line')
                 ->color('success')
                 ->chart([7, 2, 10, 3, 15, 4, 17]),
-            Stat::make('Ventas(Bs.)', 'Bs. '.number_format(VentaServicio::sum('pago_bsd'), 2, ',', '.'))
+            Stat::make('Ventas(Bs.)', 'Bs. '.number_format($ventas_bsd, 2, ',', '.'))
                 ->description('Neto de ventas en Bolívares')
                 ->descriptionIcon('heroicon-m-presentation-chart-line')
                 ->color('info')
                 ->chart([7, 2, 10, 3, 15, 4, 17]),
-            Stat::make('Total Servicios', Disponible::count())
+            Stat::make('Total Servicios', $servicios)
                 ->description('Total de servicios realizados')
                 ->descriptionIcon('heroicon-s-users')
                 ->color('gray')
-                ->chart([7, 2, 10, 3, 15, 4, 17]),
-            Stat::make('Productos', Producto::count())
-                ->description('Total de Productos en inventario')
-                ->descriptionIcon('heroicon-s-users')
-                ->color('warning')
                 ->chart([7, 2, 10, 3, 15, 4, 17]),
         ];
     }
 
     public function getColumns(): int
     {
-        return 4;
+        return 3;
     }
 }

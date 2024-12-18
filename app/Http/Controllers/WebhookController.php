3084e9b9ed4bb6a667d726c93aab08f23bc1a9d2
Gustavo <administrador@sqlapio.com>
2024-12-18 15:24:48 -0400

    se elimina restriccion para el formato de inicio del  numero telefonico

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -30,75 +30,64 @@ class WebhookController extends Controller
      * 
      * Devuelve un 200 con un mensaje de confirmacion si se agendo correctamente.
      */
-    public function webhookAgendarCita ($name, $phone, $fecha, $hora, $servicio_id) {
+    public function webhookAgendarCita($name, $phone, $fecha, $hora, $servicio_id)
+    {
 
         $fecha_api = date("Y-m-d", strtotime($fecha));
 
         $hora_formateada = date("H:i:s", strtotime($hora));
 
-        if($name == null || $phone == null || $fecha == null || $hora == null || $servicio_id == null)
-        {
+        if ($name == null || $phone == null || $fecha == null || $hora == null || $servicio_id == null) {
             return response()->json(['message' => 'Por favor llene todos los campos para poder agendar la cita'], 400);
-        }else {
-            
-            if($fecha_api >= now()->format('Y-m-d')) //Valicacion de fecha menor a la fecha actual
+        } else {
+
+            if ($fecha_api >= now()->format('Y-m-d')) //Valicacion de fecha menor a la fecha actual
             {
-                if (str_starts_with($phone, '0412') || str_starts_with($phone, '0414') || str_starts_with($phone, '0424') || str_starts_with($phone, '0416') || str_starts_with($phone, '0426')) {
-                    
-                    if (now()->format('Y-m-d') < date("Y-m-d", strtotime($fecha_api . "+ 15 days"))) {
-                        //Si la fecha de la cita es mayor a 15 dias, no se agenda la cita
-                        if ($hora_formateada >= '10:00' && $hora_formateada < '22:00') {
-                            //Si la hora de la cita es menor a 10:00am o mayor a 22:00pm, no se agenda la cita
-                            if ($servicio_id > 0) {
-                                //Si el servicio_id exite mas de 4 veces no se agrega la cita
-                                $citas = Cita::where('servicio_id', $servicio_id)
+                if (now()->format('Y-m-d') < date("Y-m-d", strtotime($fecha_api . "+ 15 days"))) {
+                    //Si la fecha de la cita es mayor a 15 dias, no se agenda la cita
+                    if ($hora_formateada >= '10:00' && $hora_formateada < '22:00') {
+                        //Si la hora de la cita es menor a 10:00am o mayor a 22:00pm, no se agenda la cita
+                        if ($servicio_id > 0) {
+                            //Si el servicio_id exite mas de 4 veces no se agrega la cita
+                            $citas = Cita::where('servicio_id', $servicio_id)
                                 ->where('hora', $hora)
-                                    ->where('status', 1)
-                                    ->count();
-    
-                                if ($citas > 4) {
-                                    return response()->json(['message' => 'No disponemos de técnico disponible para la hora que solicita la cita, por favor modifique la hora'], 400);
-                                } else {
-                                    $citas = new Cita();
-                                    $citas->cod_cita = 'Pci-' . random_int(11111, 99999);
-                                    $citas->telefono = $phone;
-                                    $citas->cliente = $name;
-                                    $citas->hora = str_replace(' ', '', $hora);
-                                    $citas->fecha = Carbon::parse($fecha_api)->isoFormat('dddd, D MMM');
-                                    $citas->fecha_formateada = $fecha_api;
-                                    $citas->responsable = 'PiedyBot';
-                                    $citas->status = 1;
-                                    $citas->sucursal_id = 1;
-                                    $citas->servicio_id = $servicio_id;
-                                    $citas->save();
-    
-                                    Log::info('Nueva cita agendada por PiedyBot: ' . $citas->cod_cita);
-                                    return response()->json(['message' => 'cita agendada'], 200);
-                                }
+                                ->where('status', 1)
+                                ->count();
+
+                            if ($citas > 4) {
+                                return response()->json(['message' => 'No disponemos de técnico disponible para la hora que solicita la cita, por favor modifique la hora'], 400);
+                            } else {
+                                $citas = new Cita();
+                                $citas->cod_cita = 'Pci-' . random_int(11111, 99999);
+                                $citas->telefono = $phone;
+                                $citas->cliente = $name;
+                                $citas->hora = str_replace(' ', '', $hora);
+                                $citas->fecha = Carbon::parse($fecha_api)->isoFormat('dddd, D MMM');
+                                $citas->fecha_formateada = $fecha_api;
+                                $citas->responsable = 'PiedyBot';
+                                $citas->status = 1;
+                                $citas->sucursal_id = 1;
+                                $citas->servicio_id = $servicio_id;
+                                $citas->save();
+
+                                Log::info('Nueva cita agendada por PiedyBot: ' . $citas->cod_cita);
+                                return response()->json(['message' => 'cita agendada'], 200);
                             }
-                        } else {
-                            //log
-                            Log::error('Error al agendar cita por PiedyBot: La hora debe estar entre las 10:00am y las 10:00pm. Por favor intente nuevamente');
-                            return response()->json(['message' => 'Nuestra hora de atención es de 10:00am a 10:00pm, por favor modifique la hora.'], 400);
                         }
                     } else {
                         //log
-                        Log::error('Error al agendar cita por PiedyBot: La fecha debe ser menor a 15 dias');
-                        return response()->json(['message' => 'La fecha debe ser menor a 15 días'], 400);
+                        Log::error('Error al agendar cita por PiedyBot: La hora debe estar entre las 10:00am y las 10:00pm. Por favor intente nuevamente');
+                        return response()->json(['message' => 'Nuestra hora de atención es de 10:00am a 10:00pm, por favor modifique la hora.'], 400);
                     }
                 } else {
                     //log
-                    Log::error('Error al agendar cita por PiedyBot: El telefono debe comenzar con 0412, 0414, 0424, 0416 o 0426');
-                    return response()->json(['message' => 'El teléfono no posee un formato valido, debe comenzar con 0412, 0414, 0424, 0416 o 0426'], 400);
+                    Log::error('Error al agendar cita por PiedyBot: La fecha debe ser menor a 15 dias');
+                    return response()->json(['message' => 'La fecha debe ser menor a 15 días'], 400);
                 }
-                   
-            }else{
+            } else {
                 return response()->json(['message' => 'La fecha debe ser mayor o igual a la fecha actual'], 400);
             }
-    
         }
-        
-
     }
     //
 }

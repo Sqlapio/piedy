daac647814f322fb3192a0204eaee9adc4706c44
Gustavo <administrador@sqlapio.com>
2024-12-26 17:06:58 -0400

    Modificaciones en el JOB de notificaciones masivas

--- app/Console/Commands/EjecutaNotificacionMasiva.php
+++ app/Console/Commands/EjecutaNotificacionMasiva.php
@@ -5,6 +5,8 @@ namespace App\Console\Commands;
 use App\Models\Cliente;
 use Illuminate\Console\Command;
 use App\Models\NotificacionMasiva;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\LogController;
 
 class EjecutaNotificacionMasiva extends Command
 {
@@ -29,16 +31,18 @@ class EjecutaNotificacionMasiva extends Command
     {
         try {
 
-            $info = NotificacionMasiva::where('fecha_programacion', date('Y-m-d'))->first();     
+            $info = NotificacionMasiva::where('fecha_programacion', date('Y-m-d'))->first();
 
-            $user_phone = Cliente::all();
+            $user_phone = Cliente::where('status', 1)->get('telefono')->toArray();
 
-            foreach ($user_phone as $value) {
-
+            for ($i = 0; $i < count($user_phone); $i++) {
+                # code...
                 $params = array(
                     'token' => env('TOKEN_API_WHATSAPP'),
-                    'to' => $value->telefono,
+                    'to' => $user_phone[$i]['telefono'],
                     'image' => env('APP_URL') . '/storage/' . $info->image,
+                    // 'image' => env('IMAGE_PROMOCION'),
+
                     'caption' => $info->caption
                 );
                 $curl = curl_init();
@@ -61,16 +65,25 @@ class EjecutaNotificacionMasiva extends Command
                 $response = curl_exec($curl);
                 $err = curl_error($curl);
 
+                $res = json_decode($response, true);
+
                 curl_close($curl);
 
-                if ($err) {
-                    $this->info("cURL Error #:" . $err);
-                } else {
-                    $this->info($response);
+                if (isset($res['sent']) and $res['sent'] == 'true') {
+                    array_push($response_ok, $res['sent']);
+                }
+
+                if (isset($res['error'])) {
+                    array_push($response_err, $res['error']);
                 }
             }
+
+            LogController::log(1, 'ejecucion job sistema', 'servicio masivo WhatsApp. Total enviados: ' . count($response_ok), $response = null);
+            LogController::log(1, 'excepcion job sistema', 'falla de servicio masivo WhatsApp. Total fallidos:' . count($response_err), $response = null);
+
             //code...
         } catch (\Throwable $th) {
+            LogController::log(1, 'excepcion job fallido', $th->getMessage(), $response = null);
         }
     }
 }

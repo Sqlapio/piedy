ab8f46ad1403357b87cf358e888ee2cd64b30e8b
karanguren <karanguren_12@outlook.com>
2025-01-02 11:16:26 -0400

    cambios en dashboard / Clientes / Area de trabajo

--- resources/views/livewire/cabinas.blade.php
+++ resources/views/livewire/cabinas.blade.php
@@ -3,10 +3,10 @@
     @livewire('notifications')
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 mb-4 mt-4 gap-4">
         @foreach ($data as $item)
-            <div id="accordion-collapse" data-accordion="collapse" class="w-full">
+            <div id="accordion-collapse" data-accordion="collapse" class="w-full" data-active-classes="bg-gray-4 00">
                 <h2 id="accordion-collapse-heading-{{ $item->id }}">
-                    <button  type="button" class="flex justify-start items-center w-full p-5 {{ $item->status == 'activo' ? 'text-white' : 'text-black' }} font-extrabold rounded-xl {{ $item->status == 'activo' ? 'bg-green-700' : 'bg-yellow-300' }} gap-3" data-accordion-target="#accordion-collapse-body-{{ $item->id }}" aria-expanded="false" aria-controls="accordion-collapse-body-{{ $item->id }}">
-                        <img class="w-20 h-auto rounded-full ml-4" src="{{ $item->status == 'activo' ? asset('images/abierto.png') : asset('images/cerrado2.png') }}" alt="">
+                    <button type="button" class="flex justify-start items-center w-full p-5 {{ $item->status == 'activo' ? 'text-white' : 'text-black' }} font-extrabold rounded-xl {{ $item->status == 'activo' ? 'bg-[#BF9C8F]' : 'bg-[#7B9AA6]' }} gap-3" data-accordion-target="#accordion-collapse-body-{{ $item->id }}" aria-expanded="false" aria-controls="accordion-collapse-body-{{ $item->id }}">
+                        <img class="w-20 h-auto rounded-full ml-4" src="{{ $item->status == 'activo' ? asset('images/abierto.png') : asset('images/cerrado.png') }}" alt="">
                         <div class="flex flex-col items-start">
                             <span class="text-sm">Cliente: {{ $item->cliente->nombre }}</span>
                             <span class="text-sm">Técnico: {{ $item->empleado->name }}</span>
@@ -17,7 +17,7 @@
                 <div id="accordion-collapse-body-{{ $item->id }}" class="hidden" aria-labelledby="accordion-collapse-heading-1">
                     <div class="border rounded-lg mb-5 mt-5 ">
                         {{-- <p class="p-4 text-3xl font-bold text-[#bc9c95]">Venta de Productos</p> --}}
-                        @livewire('table-detalle-asignacion', 
+                        @livewire('table-detalle-asignacion',
                         [
                             'cod_asignacion' => $item->cod_asignacion,
                             'cliente_id'     => $item->cliente_id

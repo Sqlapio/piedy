8f1b300d23d1fc22742689a591b7f64794ad7c54
Gustavo <administrador@sqlapio.com>
2025-01-03 23:02:11 -0400

    Modificaciones en el recordatorio al tecnico

--- app/Console/Commands/RecordatorioCitaTecnico.php
+++ app/Console/Commands/RecordatorioCitaTecnico.php
@@ -31,71 +31,66 @@ class RecordatorioCitaTecnico extends Command
         $citas = Cita::where('fecha_formateada', date('Y-m-d'))
         ->where('status', 1)
         ->get();
-        
-        
 
         foreach ($citas as $cita) {
 
-            $link_confirmar = env('LINK_CONFIRMACION').$cita->id;
-            $link_cancelar = env('LINK_CANCELACION').$cita->id;
+            if($cita->empleado_id != null){
 
-            $empleado = User::where('id', $cita->empleado_id)->first();
-        
-            $body = <<<HTML
+                $empleado = User::where('id', $cita->empleado_id)->first();
 
-            *Sr(a):* {$empleado->name}
+                $body = <<<HTML
 
-            Le recordamos que para el día de hoy tiene una cita agendada, a continuación los detalles:
+                *Sr(a):* {$empleado->name}
 
-            *Fecha:* {$cita->fecha}
-            *Hora:* {$cita->hora}
+                Le recordamos que para el día de hoy tiene una cita agendada, a continuación los detalles:
 
-            *Para confirmar su asistencia por favor ingrese al link:*
-            {$link_confirmar}
+                *Fecha:* {$cita->fecha}
+                *Hora:* {$cita->hora}
+                *Cliente:* {$cita->cliente}
+                
+                *Con el fin de respetar el tiempo de nuestros clientes, le sugerimos debe llegar puntual a la cita.*
+                HTML;
 
-            *Para cancelar su asistencia por favor ingrese al link:*
-            {$link_cancelar}
-            
-            *Con el fin de respetar el tiempo de nuestros clientes, le sugerimos debe llegar puntual a la cita.*
-            HTML;
+                $params = array(
+                    'token' => env('TOKEN_API_WHATSAPP'),
+                    'to' => $empleado->telefono,
+                    'image' => env('IMAGE'),
+                    'caption' => $body
+                );
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => env('CURLOPT_URL_IMAGE'),
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "POST",
+                    CURLOPT_POSTFIELDS => http_build_query($params),
+                    CURLOPT_HTTPHEADER => array(
+                        "content-type: application/x-www-form-urlencoded"
+                    ),
+                ));
 
-            $params = array(
-                'token' => env('TOKEN_API_WHATSAPP'),
-                'to' => $empleado->telefono,
-                'image' => env('IMAGE'),
-                'caption' => $body
-            );
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => env('CURLOPT_URL_IMAGE'),
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
-                CURLOPT_SSL_VERIFYHOST => 0,
-                CURLOPT_SSL_VERIFYPEER => 0,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS => http_build_query($params),
-                CURLOPT_HTTPHEADER => array(
-                    "content-type: application/x-www-form-urlencoded"
-                ),
-            ));
+                $response = curl_exec($curl);
+                $err = curl_error($curl);
 
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
+                $res = json_decode($response, true);
 
-            $res = json_decode($response, true);
+                curl_close($curl);
 
-            curl_close($curl);
+                if (isset($res['sent']) and $res['sent'] == 'true') {
+                    Log::info('Recordatorio enviado al tecnico con exito: ' . $response);
+                }
 
-            if (isset($res['sent']) and $res['sent'] == 'true') {
-                Log::info('Recordatorio enviado al tecnico con exito: ' . $response);
+                if (isset($res['error'])) {
+                    Log::error('Error al enviar el recordatorio: ' . $response);
+                }
+                
             }
 
-            if (isset($res['error'])) {
-                Log::error('Error al enviar el recordatorio: ' . $response);
-            }
         }
 
         $this->info($response);

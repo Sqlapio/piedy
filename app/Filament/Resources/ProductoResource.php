294fc3aba652e1889a02c254501224101ba44eab
Gustavo <administrador@sqlapio.com>
2025-01-09 14:50:52 -0400

    Add modifications to ProductoResource

--- app/Filament/Resources/ProductoResource.php
+++ app/Filament/Resources/ProductoResource.php
@@ -237,7 +237,7 @@ class ProductoResource extends Resource
 
                 TextColumn::make('contenido_neto')
                     ->toggleable(isToggledHiddenByDefault: false)
-
+                    ->numeric()
                     ->alignCenter()
                     ->searchable(),
 

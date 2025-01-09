efe4f0175cd0be14610681b0a40d064accc98562
Gustavo <administrador@sqlapio.com>
2025-01-09 12:59:58 -0400

    Add modifications to InventarioSucursalResource

--- app/Filament/Resources/InventarioSucursalResource.php
+++ app/Filament/Resources/InventarioSucursalResource.php
@@ -56,6 +56,11 @@ class InventarioSucursalResource extends Resource
         return $table
             ->query(InventarioSucursal::query()->orderBy('created_at', 'desc'))
             ->columns([
+                Tables\Columns\TextColumn::make('cod_producto')
+                ->icon('heroicon-s-shopping-bag')
+                ->numeric()
+                    ->searchable()
+                    ->sortable(),
                 Tables\Columns\TextColumn::make('producto.descripcion')
                     ->icon('heroicon-s-shopping-bag')
                     ->numeric()

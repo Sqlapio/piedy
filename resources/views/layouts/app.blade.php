a6556e1f7550f33edcb9f644b967d664397abbff
Gustavo <administrador@sqlapio.com>
2025-01-14 09:41:09 -0400

    Subiendo favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,70 +1,71 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" sizes="256x256" href="{{ asset('images/favicon2.png') }}">
-        <link rel="icon" sizes="180x180" href="{{ asset('images/favicon2.png') }}">
+    <link rel="icon" sizes="256x256" href="{{ asset('images/favicon.ico') }}">
+    <link rel="icon" sizes="180x180" href="{{ asset('images/favicon.ico') }}">
 
-        <link rel="apple-touch-icon" href="{{ asset('images/favicon2.png') }}"/>
+    <link rel="apple-touch-icon" href="{{ asset('images/favicon.ico') }}" />
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
 
-        <style>
-            [x-cloak] {
-                display: none !important;
-            }
-        </style>
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
 
-        <!-- wireUI -->
-        <wireui:scripts />
+    </style>
 
-        @filamentStyles
+    <!-- wireUI -->
+    <wireui:scripts />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css'])
+    @filamentStyles
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-notifications z-index="z-50" />
-        <x-dialog z-index="z-50" blur="md" align="center" />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css'])
 
-        <x-banner />
-        <div class="mx-auto min-h-screen">
-            @livewire('navigation-menu')
+    <!-- Styles -->
+    @livewireStyles
+</head>
+<body class="font-sans antialiased">
+    <x-notifications z-index="z-50" />
+    <x-dialog z-index="z-50" blur="md" align="center" />
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+    <x-banner />
+    <div class="mx-auto min-h-screen">
+        @livewire('navigation-menu')
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+        <!-- Page Heading -->
+        @if (isset($header))
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+        @endif
 
-        @filamentScripts
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
 
-        @stack('modals')
+    @filamentScripts
 
-        @livewire('livewire-ui-modal')
+    @stack('modals')
 
-        @livewireScripts
+    @livewire('livewire-ui-modal')
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
-    </body>
+    @livewireScripts
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+</body>
 </html>

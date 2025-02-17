fda1aca2a5a91fd65b1b3b2b15cd18cf7eb0f1e2
Gustavo <administrador@sqlapio.com>
2025-02-17 14:13:32 -0400

    corrigiendo, agregando las propinas al cierre

--- app/Http/Controllers/CierreDiarioController.php
+++ app/Http/Controllers/CierreDiarioController.php
@@ -35,7 +35,7 @@ class CierreDiarioController extends Controller
                 $total_efectivo_usd_productos   = VentaProducto::where('fecha_venta', date('d-m-Y'))->where('metodoUsd', 'Efectivo Usd')->sum('montoUsd');
                 $total_zelle                    = VentaServicio::where('fecha_venta', date('d-m-Y'))->where('metodo_pago', 'Zelle')->sum('pago_usd');
                 $total_zelle_productos          = VentaProducto::where('fecha_venta', date('d-m-Y'))->where('metodoUsd', 'Zelle')->sum('montoUsd');
-                $total_propinas_usd             = VentaProducto::where('fecha_venta', date('d-m-Y'))->sum('propina_usd');
+                $total_propinas_usd             = VentaServicio::where('fecha_venta', date('d-m-Y'))->sum('propina_usd');
 
                 /** totales de pagos en Bolivares*/
                 $total_bs                   = VentaServicio::where('fecha_venta', date('d-m-Y'))->sum('pago_bsd');
@@ -43,7 +43,7 @@ class CierreDiarioController extends Controller
                 $total_pago_movil_bsd       = VentaServicio::where('fecha_venta', date('d-m-Y'))->where('metodo_pago_dos', 'Pago movil')->sum('pago_bsd');
                 $total_punto_venta_bsd      = VentaServicio::where('fecha_venta', date('d-m-Y'))->where('metodo_pago_dos', 'Punto de venta')->sum('pago_bsd');
                 $total_transferencia_bsd    = VentaServicio::where('fecha_venta', date('d-m-Y'))->where('metodo_pago_dos', 'Transferencia')->sum('pago_bsd');
-                $total_propinas_bsd         = VentaProducto::where('fecha_venta', date('d-m-Y'))->sum('propina_bsd');
+                $total_propinas_bsd         = VentaServicio::where('fecha_venta', date('d-m-Y'))->sum('propina_bsd');
 
                 /** Total de pago en bolivares para venta de productos */
                 $totalprod_bsd               = VentaProducto::where('fecha_venta', date('d-m-Y'))->sum('montoBsd');

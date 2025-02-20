a2541b088f61bab104a56fa03dcf8a45f05a9a1f
Gustavo <administrador@sqlapio.com>
2025-02-20 17:30:56 -0400

    subiendo label de tablas de libro de compra

--- app/Filament/Resources/LibroCompraResource.php
+++ app/Filament/Resources/LibroCompraResource.php
@@ -27,89 +27,123 @@ class LibroCompraResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('gasto_id')
+                ->label('Numero Operacion')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('sucursal.nombre')
+                ->label('Sucursal')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('rif')
+                ->label('Rif')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('razon_social')
+                ->label('Razon Social')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('tipo_prov')
+                ->label('Tipo Proveedor')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('nro_comprobante')
+                ->label('Nro Comprobante')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('fecha_documento')
+                ->label('Fecha Documento')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('fecha_apli_retencion')
+                    ->label('Fecha Retencion')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('nro_planilla_importacion')
+                    ->label('Nro Planilla Importacion')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('nro_expediente_importacion')
+                    ->label('Nro Expediente Importacion')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('nro_decla_aduana')
+                    ->label('Nro Declaracion Aduana')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('fecha_decla_aduana')
+                    ->label('Fecha Declaracion Aduana')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('nro_documento')
+                    ->label('Nro Documento')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('nro_control')
+                    ->label('Nro Control')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('nro_nota_debito')
+                    ->label('Nro Nota Debito')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('nro_nota_credito')
+                    ->label('Nro Nota Credito')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('tipo_transaccion')
+                    ->label('Tipo Transaccion')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('nro_doc_afectado')
+                    ->label('Nro Doc Afectado')    
                     ->searchable(),
                 Tables\Columns\TextColumn::make('total_importacion_con_iva')
+                    ->label('Total Importacion Con Iva')    
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('impor_exenta_exoneradas')
+                    ->label('Impor Exenta Exoneradas')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('base_imponible_importaciones, 8, 2')
+                    ->label('Base Imponible Importaciones')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('porcen_alicuota_importaciones')
+                    ->label('Porcen Alicuota Importaciones')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('impuesto_iva_importaciones')
+                    ->label('Impuesto Iva Importaciones')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('total_comp_con_iva')
+                    ->label('Total Comp Con Iva')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('comp_sin_derecho_credito')
+                    ->label('Comp Sin Derecho Credito')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('compras_exentas')
+                    ->label('Compras Exentas')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('compras_exoneradas')
+                    ->label('Compras Exoneradas')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('compras_no_sujetas')
+                    ->label('Compras No Sujetas')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('base_imponible_internas')
+                    ->label('Base Imponible Internas')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('porcen_alicuota_internas')
+                    ->label('Porcen Alicuota Internas')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('impuesto_iva_internas')
+                    ->label('Impuesto Iva Internas')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('responsable')
+                    ->label('Responsable')    
                     ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
+                    ->label('Fecha de Creación')    
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Fecha de Actualizacion')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),

cea2272f99841678af852dd4a4196364dd9cb55a
karanguren <karanguren_12@outlook.com>
2025-02-28 18:03:12 -0400

    tasa imagen

--- resources/views/topbar-end-bcv.blade.php
+++ resources/views/topbar-end-bcv.blade.php
@@ -2,7 +2,7 @@
     <img id="theme-mode"
      x-data="{ darkMode: document.documentElement.classList.contains('dark') }"
      x-init="$watch('darkMode', value => $el.src = value ? '{{ asset('images/BCV.png') }}' : '{{ asset('images/BCV2.png') }}')"
-     :src="darkMode ? '{{ asset('images/BCV.png') }}' : '{{ asset('images/BCV.png') }}'"
+     :src="darkMode ? '{{ asset('images/BCV.png') }}' : '{{ asset('images/BCV2.png') }}'"
      class="w-20 h-auto p-3 rounded-full"
      alt="Logo">
     <div class="ml-2 text-left">

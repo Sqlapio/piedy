22333c378cea9f4494a1a764f06d64ae58b88872
Gustavo <administrador@sqlapio.com>
2025-02-28 20:15:06 -0400

    subiendo tasa bcv en dashboard

--- resources/views/topbar-end-bcv.blade.php
+++ resources/views/topbar-end-bcv.blade.php
@@ -1,13 +1,18 @@
+@php
+    use App\Models\TasaBcv;
+    $tasa = TasaBcv::first();
+
+@endphp
 <div class="flex items-center">
     <img id="theme-mode"
      x-data="{ darkMode: document.documentElement.classList.contains('dark') }"
      x-init="$watch('darkMode', value => $el.src = value ? '{{ asset('images/BCV.png') }}' : '{{ asset('images/BCV2.png') }}')"
      :src="darkMode ? '{{ asset('images/BCV.png') }}' : '{{ asset('images/BCV2.png') }}'"
-     class="w-20 h-auto p-3 rounded-full"
+     class="w-10 h-auto p-0.5 rounded-full"
      alt="Logo">
     <div class="ml-2 text-left">
-        <div class="mt-2 text-sm text-black leading-7 font-bold">
-            BCV: 55.44Bs
+        <div class="ml-2 text-xs text-black leading-7 font-semibold">
+            {{ $tasa->tasa }}Bs.
         </div>
     </div>
 </div>

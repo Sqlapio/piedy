0883b5b138c064a24f8a52bfee50575fd66cb414
Gustavo <administrador@sqlapio.com>
2025-03-11 09:15:13 -0400

    segundo cambio para pruebas

--- app/Http/Controllers/NotificacionesController.php
+++ app/Http/Controllers/NotificacionesController.php
@@ -297,7 +297,7 @@ class NotificacionesController extends Controller
                 $params = array(
                     'token' => env('TOKEN_API_WHATSAPP'),
                     // 'to' => $user_phone[$i]['telefono'],
-                    'to' => '4127018390',
+                    'to' => '4247667265',
                     'image' => env('APP_URL') . '/storage/' . $image,
                     // 'image' => env('IMAGE_PROMOCION'),
 
